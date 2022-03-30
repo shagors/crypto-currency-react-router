bc8bbbdd6afb540eec6cbb8a67db697e0753e060
shagors <shajjadshagor@gmil.com>
2022-03-30 21:17:13 +0600

    page responsive

--- src/components/CoinDetails/CoinDetails.js
+++ src/components/CoinDetails/CoinDetails.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner';
 
 const CoinDetails = () => {
     const { id } = useParams();
     const [coin, setCoin] = useState({});
-    console.log(coin);
+    const [loading, setLoading] = useState(false);
     useEffect(()=>{
+        setLoading(true);
         const url = `https://api.coingecko.com/api/v3/coins/${id}`;
         fetch(url)
         .then(res => res.json())
         .then(data => setCoin(data));
+        setLoading(false);
     }, [id]);
     return (
-        <div className='px-4 h-[70vh] pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
+        <>
+        {loading ? <Spinner></Spinner> : 
+            <div className='px-4 h-[70vh] pt-20 pb-24 mx-auto max-w-7xl md:px-2'>
             <div className='h-full grid grid-cols-1 md:grid-cols-2 justify-items-center gap-4 content-center'>
                 <div className='order-2 md:order-1'>
                     <h1 className='text-3xl'>General Info :</h1>
@@ -24,13 +29,16 @@ const CoinDetails = () => {
                     <hr />
                     <h1>Community Score : {coin.community_score}</h1>
                     <h1>Developer Score: {coin.developer_score}</h1>
-                    <h1>Origin : {coin.country_origin ? coin.country_origin : "Not Found"}</h1>
+                    <h1>Public Interest : {coin.public_interest_score}</h1>
                 </div>
                 <div className='flex justify-center items-center order-1 md:order-2'>
                     <img src={coin.image?.large} alt="" />
                 </div>
             </div>
         </div>
+        }
+
+        </>
     );
 };
 
